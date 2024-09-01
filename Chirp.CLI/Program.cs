1e3962732ab02185781ac65cab25fc4c26281348
Nicklas Lundsteen Jensen <122295673+Lund5teen@users.noreply.github.com>
2024-09-01 10:51:07 +0200

    Simple Chirping
    Added basic chirping functionality described in session 1

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+
+string path = "chirp_cli_db.csv";
+
+if (args[0] == "cheep")
+{
+    string userName = Environment.UserName;
+    DateTimeOffset time = DateTimeOffset.Now;
+    string message = args[1];
+    
+    using (StreamWriter sw = File.AppendText(path))
+    {
+        sw.WriteLine(userName + " @ " + time.ToUniversalTime() + ": " + message);
+    }
+}
+
+else if (args[0] == "read")
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        string s = "";
+        while ((s = sr.ReadLine()) != null)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
+
+else
+{
+    Console.WriteLine(args[0] ?? "Unknown command");
+}
