0191bebee1297335a1a6800c83a266c4ae1d4cf5
Teis <teif@itu.dk>
2024-09-20 11:49:53 +0200

    Cloned database to webapp.
    program.cs currently contains a few attempts to read csv to json,
     they are  commented out as they do not work.

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -1,6 +1,18 @@
+using CsvHelper;
+using SimpleDB;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/cheeps", () => "here we doth reatethht thine cheeps");
+app.MapGet("/cheeps", () => new[]
+    {
+            new {name = "pølse"}, 
+            /*         foreach (var cheep in database.Read()){
+                {new name = $"{cheep.Author}",timestamp =  "{cheep.Timestamp}", content = $"{cheep.Message}"}
+            }*/
+    });
+        
 app.MapGet("/cheep", () => "ein requeste hereth postth thine cheep to yon csv DB");
 app.Run();
+
+IDatabaseRepository<Cheep> database = CSVDatabase<Cheep>.Instance;
+public record Cheep(string Author, string Message, long Timestamp);
