c14d6415f7a7eb4e8391eba5514068d00668b469
Teis <teif@itu.dk>
2024-09-20 11:14:25 +0200

    Some initial setup done
    imported some necessary .csproj things.
    
    put some placefolder requests in program.cs

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/cheeps", () => "here we doth reatethht thine cheeps");
+app.MapGet("/cheep", () => "ein requeste hereth postth thine cheep to yon csv DB");
 app.Run();
