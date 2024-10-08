74440d42d965213feaa9694eeed672174b33ff23
Teis <teif@itu.dk>
2024-10-08 15:45:10 +0200

    release version
    Changed a comment to activate the release workflow.

--- src/Chirp.Razor/CheepService.cs
+++ src/Chirp.Razor/CheepService.cs
@@ -36,7 +36,7 @@ public class CheepService : ICheepService
 
     private static string UnixTimeStampToDateTimeString(double unixTimeStamp)
     {
-        // Unix timestamp is seconds past epoch
+        // Unix timestamp is seconds past epoch.
         DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
         dateTime = dateTime.AddSeconds(unixTimeStamp);
         return dateTime.ToString("MM/dd/yy H:mm:ss");
