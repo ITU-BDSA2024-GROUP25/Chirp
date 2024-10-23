93e613f320721caa7c5fbc70114566740345fef8
kilzor33 <lukas.hammerhoj@gmail.com>
2024-10-23 15:44:50 +0200

    Co-authored-by: Carla <cmoo@itu.dk> Co-authored-by: Marie <mblh@itu.dk> Co-authored-by: Nicklas <nlje@itu.dk> Co-authored-by: Teis <teif@itu.dk>

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -1,6 +1,6 @@
 using Chirp.Infrastructure;
 using Chirp.Core;
-
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +11,21 @@ builder.Services.AddDbContext<ChirpDbContext>(options => options.UseSqlite(conne
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<ICheepService, CheepService>();
 builder.Services.AddScoped<ICheepRepository, CheepRepository>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = "GitHub";
+    })
+    .AddCookie()
+  
+    .AddGitHub(o =>
+    {
+        o.ClientId = builder.Configuration["authentication:github:clientId"];
+        o.ClientSecret = builder.Configuration["authentication:github:clientSecret"];
+        o.CallbackPath = "/signin-github";
+    });
 
 
 var app = builder.Build();
@@ -23,13 +38,17 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapRazorPages();
+
+//app.UseSession();
 
 app.Run();
 
