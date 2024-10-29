78f1ddf6cc059caaf54d8ca730cdbf0e5f7ce95c
Nicklas Lundsteen Jensen <122295673+Lund5teen@users.noreply.github.com>
2024-10-29 13:57:45 +0100

    Current Version of login
    Co-authored-by: Lukas <lukha@itu.dk>

---
+++ src/Chirp.Web/Pages/Shared/_LoginPartial.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Web.Pages;
+
+public class _LoginPartial : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -1,42 +1,44 @@
 using Chirp.Infrastructure;
 using Chirp.Core;
-
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ChirpDbContext>(options => options.UseSqlite(connectionString, b => b.MigrationsAssembly("Chirp.Web")));
-//DbInitializer.SeedDatabase(Context);
-// Add services to the container.
+
+builder.Services.AddDbContext<ChirpDbContext>(options =>
+    options.UseSqlite(connectionString, b => b.MigrationsAssembly("Chirp.Web")));
+
+// Add services to the container
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<ICheepService, CheepService>();
 builder.Services.AddScoped<ICheepRepository, CheepRepository>();
 
 builder.Services.AddDefaultIdentity<AppUser>(options =>
-{
-    // Making sure our password can be "password" like god intended!
-    // also verifying emails is for cowards.
-    options.SignIn.RequireConfirmedAccount = false;
-    options.Password.RequiredLength = 8;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireDigit = false;
-}).AddEntityFrameworkStores<ChirpDbContext>();
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+        options.Password.RequiredLength = 8;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireDigit = false;
+    })
+    .AddEntityFrameworkStores<ChirpDbContext>()
+    .AddDefaultUI(); 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();    
+app.UseAuthorization();     
 
 app.MapRazorPages();
 
