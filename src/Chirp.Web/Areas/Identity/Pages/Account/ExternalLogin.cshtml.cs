2c51f381621250bb83dfc2f3ae3a64c7261a1c19
Teis <teif@itu.dk>
2024-11-15 14:29:15 +0100

    Removed more stuff
    this has more input related things deleted.
    And is still functional.
    also made the confirm page less confusing.

--- src/Chirp.Web/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -50,8 +50,7 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        [BindProperty]
-        public InputModel Input { get; set; }
+  
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -72,19 +71,6 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
         [TempData]
         public string ErrorMessage { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public class InputModel
-        {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
-            [EmailAddress]
-            public string Email { get; set; }
-        }
         
         public IActionResult OnGet() => RedirectToPage("./Login");
 
