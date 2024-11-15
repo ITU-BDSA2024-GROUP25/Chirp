2d9356c765aad490d668c60af8917d5bd24eb94f
Marie <mblh@itu.dk>
2024-11-15 14:58:54 +0100

    New branch for creating new tests

--- test/PlaywrightTests/EndToEndTest.cs
+++ test/PlaywrightTests/EndToEndTest.cs
@@ -1,4 +1,6 @@
 using Microsoft.Playwright;
+using NUnit;
+using static Microsoft.Playwright.Assertions;
 
 namespace PlaywrightTests;
 
@@ -19,5 +21,26 @@ public class EndToEndTest
         await page.GotoAsync("https://bdsagroup25chirprazor1.azurewebsites.net/");
         await page.Locator("p").Filter(new() { HasText = "Jacqualine Gilcoine Starbuck" }).GetByRole(AriaRole.Link).ClickAsync();
         await page.GetByRole(AriaRole.Heading, new() { Name = "Jacqualine Gilcoine's Timeline" }).ClickAsync();
+    }
+
+    [Test]
+    public async Task IsolateAuthorCheepsTest1()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false,
+        });
+        var context = await browser.NewContextAsync();
+
+        var page = await context.NewPageAsync();
+        await page.GotoAsync("https://bdsagroup25chirprazor1.azurewebsites.net/");
+        await page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
+        await page.GetByPlaceholder("name@example.com").ClickAsync();
+        await page.GetByPlaceholder("name@example.com").FillAsync("test@test.dk");
+        await page.GetByPlaceholder("password").ClickAsync();
+        await page.GetByPlaceholder("password").FillAsync("password");
+        await page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+        await Expect(page.Locator("body")).ToContainTextAsync("logout [test@test.dk]");
     }
 }
