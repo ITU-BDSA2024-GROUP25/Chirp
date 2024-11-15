72c09d644b37da41797d4b9992de7e5280c90bef
Marie <mblh@itu.dk>
2024-11-15 15:17:53 +0100

    Body for making new cheeptest
    As this was tried to be implemented we discovered a bug where the user is no long able to login to a newly registered user

--- test/PlaywrightTests/EndToEndTest.cs
+++ test/PlaywrightTests/EndToEndTest.cs
@@ -23,24 +23,11 @@ public class EndToEndTest
         await page.GetByRole(AriaRole.Heading, new() { Name = "Jacqualine Gilcoine's Timeline" }).ClickAsync();
     }
 
+    //tester@test.dk
     [Test]
-    public async Task IsolateAuthorCheepsTest1()
+    public async Task ValidCheepTest()
     {
-        using var playwright = await Playwright.CreateAsync();
-        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-        {
-            Headless = false,
-        });
-        var context = await browser.NewContextAsync();
-
-        var page = await context.NewPageAsync();
-        await page.GotoAsync("https://bdsagroup25chirprazor1.azurewebsites.net/");
-        await page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
-        await page.GetByPlaceholder("name@example.com").ClickAsync();
-        await page.GetByPlaceholder("name@example.com").FillAsync("test@test.dk");
-        await page.GetByPlaceholder("password").ClickAsync();
-        await page.GetByPlaceholder("password").FillAsync("password");
-        await page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
-        await Expect(page.Locator("body")).ToContainTextAsync("logout [test@test.dk]");
+        
+        
     }
 }
