acef1133032db2e8a84bb0bc00f157d06fcba465
Marie <mblh@itu.dk>
2024-11-22 15:40:04 +0100

    Revert "Adjusted unittest so they work with the aplitted author and cheep reposotory"
    This reverts commit 3fb4c7d23581e6f3740b9c6ad286c6a1c27c38fb.

--- src/Chirp.Core/Repositories/IAuthorRepository.cs
+++ src/Chirp.Core/Repositories/IAuthorRepository.cs
@@ -4,6 +4,7 @@ public interface IAuthorRepository
 {
     // Queries
     public Task<Author?> FindAuthorByName(string name);
+    public Task<Author?> FindAuthorByEmail(string email);
     public Task<Author?> GetAuthorById (int id);
     public Task<bool> IsFollowing(string userName, string targetUserName);
     public Task<IList<AuthorDto>> GetFollowers(string authorName);
