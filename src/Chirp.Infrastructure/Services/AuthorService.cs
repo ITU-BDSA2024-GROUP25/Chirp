2e656524f88646d627c2b9b2fd7171583d7a3a7d
Nicklas Lundsteen Jensen <122295673+Lund5teen@users.noreply.github.com>
2024-12-15 17:51:02 +0100

    Services commented

--- src/Chirp.Infrastructure/Services/AuthorService.cs
+++ src/Chirp.Infrastructure/Services/AuthorService.cs
@@ -7,13 +7,22 @@ namespace Chirp.Infrastructure;
 /// </summary>
 public interface IAuthorService
 {
-        public Task<Author> FindAuthorByName(string name);
-        public Task CreateAuthor(AuthorDto authorDto);
-        public Task FollowAuthor(string userName, string targetUserName);
-        public Task<bool> IsFollowing(string userName, string targetUserName);
-        public Task UnfollowAuthor(string userName, string targetUserName);
-        public Task<IList<AuthorDto>> GetFollowers(string authorName);
-        public Task DeleteAuthor(string authorName);
+    #region Queries
+
+    public Task<Author> FindAuthorByName(string name);
+    public Task<bool> IsFollowing(string userName, string targetUserName);
+    public Task<IList<AuthorDto>> GetFollowers(string authorName);
+
+    #endregion
+
+    #region Commands
+
+    public Task CreateAuthor(AuthorDto authorDto);
+    public Task FollowAuthor(string userName, string targetUserName);
+    public Task UnfollowAuthor(string userName, string targetUserName);
+    public Task DeleteAuthor(string authorName);
+
+    #endregion
 }
 
 public class AuthorService : IAuthorService
@@ -24,11 +33,20 @@ public class AuthorService : IAuthorService
             _authorRepo = new AuthorRepository(context);
         }
         
+        #region Queries
+
         public async Task<Author> FindAuthorByName(string name) => await _authorRepo.FindAuthorByName(name);
+        public Task<bool> IsFollowing(string userName, string targetUserName) => _authorRepo.IsFollowing(userName, targetUserName);
+        public async Task<IList<AuthorDto>> GetFollowers(string authorName) => await _authorRepo.GetFollowers(authorName);
+
+        #endregion
+
+        #region Commands
+
         public Task CreateAuthor(AuthorDto authorDto) => _authorRepo.CreateAuthor(authorDto);
         public Task FollowAuthor(string userName, string targetUserName) => _authorRepo.FollowAuthor(userName, targetUserName);
-        public Task<bool> IsFollowing(string userName, string targetUserName) => _authorRepo.IsFollowing(userName, targetUserName);
         public Task UnfollowAuthor(string userName, string targetUserName) => _authorRepo.UnfollowAuthor(userName, targetUserName);
-        public async Task<IList<AuthorDto>> GetFollowers(string authorName)=> await _authorRepo.GetFollowers(authorName);
         public Task DeleteAuthor(string authorName) => _authorRepo.DeleteAuthor(authorName);
+
+        #endregion
 }
--- src/Chirp.Infrastructure/Services/CheepService.cs
+++ src/Chirp.Infrastructure/Services/CheepService.cs
@@ -9,22 +9,31 @@ public record CheepViewModel(string Author, string Message, string Timestamp);
 /// </summary>
 public interface ICheepService
 {
+    #region Queries
+
     public Task<List<CheepDto>> GetCheeps(string? author, int pageNumber);
     public int GetTotalCheepsCount(string? author);
-    public Task CreateCheep(CheepDto cheep, string authorName);
     public Task<List<CheepDto>> GetCheepsFromFollowers(string authorName, IList<AuthorDto> followers, int pageNumber);
     public Task<List<CheepDto>> GetAllCheeps(string author);
-    public Task DeleteCheep(CheepDto cheep);
     public Task<bool> IsCheepLikedByAuthor(string authorName, CheepDto cheep);
     public Task<bool> IsCheepDislikedByAuthor(string authorName, CheepDto cheep);
-    public Task LikeCheep(string authorName, CheepDto cheep);
-    public Task DislikeCheep(string authorName, CheepDto cheep);
-    public Task RemoveLikeCheep(string authorName, CheepDto cheep);
-    public Task RemoveDislikeCheep(string authorName, CheepDto cheep);
     public Task<int> GetCheepLikesCount(CheepDto cheep);
     public Task<int> GetCheepDislikesCount(CheepDto cheep);
     public Task<List<CheepDto>> GetLikedCheeps(string authorName);
     public Task<List<CheepDto>> GetDislikedCheeps(string authorName);
+
+    #endregion
+
+    #region Commands
+
+    public Task CreateCheep(CheepDto cheep, string authorName);
+    public Task DeleteCheep(CheepDto cheep);
+    public Task LikeCheep(string authorName, CheepDto cheep);
+    public Task DislikeCheep(string authorName, CheepDto cheep);
+    public Task RemoveLikeCheep(string authorName, CheepDto cheep);
+    public Task RemoveDislikeCheep(string authorName, CheepDto cheep);
+
+    #endregion
 }
 
 public class CheepService : ICheepService
@@ -36,20 +45,32 @@ public class CheepService : ICheepService
         _cheepRepo = new CheepRepository(context);
     }
     
-    public int GetTotalCheepsCount(string? author) => _cheepRepo.GetTotalCheepsCount(author);
+    #region Queries
+    
     public Task<List<CheepDto>> GetCheeps(string? author, int pageNumber) => _cheepRepo.GetCheeps(author, pageNumber);
-    public Task CreateCheep(CheepDto cheep, string authorName) => _cheepRepo.CreateCheep(cheep, authorName);
-    public Task<List<CheepDto>> GetCheepsFromFollowers(string authorName, IList<AuthorDto> followers, int pageNumber) => _cheepRepo.GetCheepsFromFollowers(authorName, followers, pageNumber);
+    public int GetTotalCheepsCount(string? author) => _cheepRepo.GetTotalCheepsCount(author);
+    public Task<List<CheepDto>> GetCheepsFromFollowers(string authorName, IList<AuthorDto> followers, int pageNumber) =>
+        _cheepRepo.GetCheepsFromFollowers(authorName, followers, pageNumber);
     public Task<List<CheepDto>> GetAllCheeps(string author) => _cheepRepo.GetAllCheeps(author);
+    public Task<bool> IsCheepLikedByAuthor(string authorName, CheepDto cheep) =>
+        _cheepRepo.IsCheepLikedByAuthor(authorName, cheep);
+    public Task<bool> IsCheepDislikedByAuthor(string authorName, CheepDto cheep) =>
+        _cheepRepo.IsCheepDislikedByAuthor(authorName, cheep);
+    public Task<int> GetCheepLikesCount(CheepDto cheep) => _cheepRepo.GetCheepLikesCount(cheep);
+    public Task<int> GetCheepDislikesCount(CheepDto cheep) => _cheepRepo.GetCheepDislikesCount(cheep);
+    public Task<List<CheepDto>> GetLikedCheeps(string authorName) => _cheepRepo.GetLikedCheeps(authorName);
+    public Task<List<CheepDto>> GetDislikedCheeps(string authorName) => _cheepRepo.GetDislikedCheeps(authorName);
+
+    #endregion
+
+    #region Commands
+
+    public Task CreateCheep(CheepDto cheep, string authorName) => _cheepRepo.CreateCheep(cheep, authorName);
     public Task DeleteCheep(CheepDto cheep) => _cheepRepo.DeleteCheep(cheep);
-    public Task<bool> IsCheepLikedByAuthor(string authorName, CheepDto cheep) => _cheepRepo.IsCheepLikedByAuthor(authorName, cheep);
-    public Task<bool> IsCheepDislikedByAuthor(string authorName, CheepDto cheep) => _cheepRepo.IsCheepDislikedByAuthor(authorName, cheep);
     public Task LikeCheep(string authorName, CheepDto cheep) => _cheepRepo.LikeCheep(authorName, cheep);
     public Task DislikeCheep(string authorName, CheepDto cheep) => _cheepRepo.DislikeCheep(authorName, cheep);
     public Task RemoveLikeCheep(string authorName, CheepDto cheep) => _cheepRepo.RemoveLikeCheep(authorName, cheep);
     public Task RemoveDislikeCheep(string authorName, CheepDto cheep) => _cheepRepo.RemoveDislikeCheep(authorName, cheep);
-    public Task<int> GetCheepLikesCount(CheepDto cheep) => _cheepRepo.GetCheepLikesCount(cheep);
-    public Task<int> GetCheepDislikesCount(CheepDto cheep) => _cheepRepo.GetCheepDislikesCount(cheep);
-    public Task<List<CheepDto>> GetLikedCheeps(string authorName) => _cheepRepo.GetLikedCheeps(authorName); 
-    public Task<List<CheepDto>> GetDislikedCheeps(string authorName) => _cheepRepo.GetDislikedCheeps(authorName);
+
+    #endregion
 }
