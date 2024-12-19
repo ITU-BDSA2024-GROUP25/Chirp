e05a39dde255252c1c936c3339fd9e44ed0ec443
Carla <cmoo@itu.dk>
2024-12-19 20:30:33 +0100

    Added Like and dislike test

--- test/Chirp.Tests/UnitTests.cs
+++ test/Chirp.Tests/UnitTests.cs
@@ -349,6 +349,77 @@ public class UnitTests
         Assert.Equal(1, cheepAmount); 
     }
     
+    [Fact]
+    public async void acheepCanBeLiked_shouldReturnLikedCheep()
+    {
+        // Arrange 
+        using var connection = new SqliteConnection("Filename=:memory:");
+        await connection.OpenAsync();
+        var builder = new DbContextOptionsBuilder<ChirpDbContext>().UseSqlite(connection);
+
+        using var context = new ChirpDbContext(builder.Options);
+        await context.Database.EnsureCreatedAsync(); // Applies the schema to the database
+
+        authorRepository = new AuthorRepository(context);
+        cheepRepository = new CheepRepository(context);
+
+        context.Cheeps.ExecuteDelete();
+        context.Authors.ExecuteDelete();
+
+        AuthorDto author = new AuthorDto( "John Doe", "John+Doe@hotmail.com");
+        var newAuthor = authorRepository.CreateAuthor(author);
+
+        CheepDto cheepDto1 = new CheepDto("test cheep", "2023-08-01 13:15:22","John Doe");
+        CheepDto cheepDto2 = new CheepDto("test cheep2", "2023-08-01 13:15:24","John Doe");
+
+        await cheepRepository.CreateCheep(cheepDto1, "John Doe");
+        await cheepRepository.CreateCheep(cheepDto2, "John Doe");
+        
+        // Act 
+        await cheepRepository.LikeCheep("John Doe", cheepDto1);
+
+        var checkLikes = await cheepRepository.GetCheepLikesCount(cheepDto1);   
+        await context.SaveChangesAsync();
+        
+        // Assertæ
+        Assert.Equal(1, checkLikes);
+    }
+    [Fact]
+    public async void acheepCanBeDisLiked_shouldReturnDisLikedCheep()
+    {
+        // Arrange 
+        using var connection = new SqliteConnection("Filename=:memory:");
+        await connection.OpenAsync();
+        var builder = new DbContextOptionsBuilder<ChirpDbContext>().UseSqlite(connection);
+
+        using var context = new ChirpDbContext(builder.Options);
+        await context.Database.EnsureCreatedAsync(); // Applies the schema to the database
+
+        authorRepository = new AuthorRepository(context);
+        cheepRepository = new CheepRepository(context);
+
+        context.Cheeps.ExecuteDelete();
+        context.Authors.ExecuteDelete();
+
+        AuthorDto author = new AuthorDto( "John Doe", "John+Doe@hotmail.com");
+        var newAuthor = authorRepository.CreateAuthor(author);
+
+        CheepDto cheepDto1 = new CheepDto("test cheep", "2023-08-01 13:15:22","John Doe");
+        CheepDto cheepDto2 = new CheepDto("test cheep2", "2023-08-01 13:15:24","John Doe");
+
+        await cheepRepository.CreateCheep(cheepDto1, "John Doe");
+        await cheepRepository.CreateCheep(cheepDto2, "John Doe");
+        
+        // Act 
+        await cheepRepository.DislikeCheep("John Doe", cheepDto1);
+
+        var checkDisLikes = await cheepRepository.GetCheepDislikesCount(cheepDto1); 
+        await context.SaveChangesAsync();
+        
+        // Assertæ
+        Assert.Equal(1, checkDisLikes);
+    }
+
     /// <summary>
     /// This should test that when a author deletes a cheep in a list the cheepID is no longer in use.
     /// This is an integration test, since it is backend, it is located here. 
